18067c3cd4fc2327d1d37315206629d250768967
Kikamn <157001435+Kikamn@users.noreply.github.com>
2024-02-21 14:05:39 +0700

    Update testUserApi.java
    bagian post gk ada tadi

--- src/test/java/testApi/testUserApi.java
+++ src/test/java/testApi/testUserApi.java
@@ -1,6 +1,8 @@
 package testApi;
 
 import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -14,5 +16,31 @@ public class reqUserApi {
                 .get("https://gorest.co.in/public/v2/users") //masukan link api
                 .then().log().all()
                 .assertThat().statusCode(200);
+    }
+    @Test
+    public void postListUser(){
+            String valueName = "Sinta";
+            String valueEmail = "sinta@gmail.com";
+            String valueGender = "female";
+            String valueStatus = "active";
+
+            JSONObject obj = new JSONObject();
+            obj.put("name", valueName); // ambil sesuai dengan case
+            obj.put("email", valueEmail);
+            obj.put("gender", valueGender);
+            obj.put("status", valueStatus);
+
+            RestAssured.given()
+                    .header("Content-type", "application/json")
+                    .header("Accept", "application/json")
+                    .body(obj.toString())
+                    .when()
+                    .post("https://gorest.co.in/public/v2/users")
+                    .then().log().all()
+                    .assertThat().statusCode(201)
+                    .assertThat().body("name", Matchers.equalTo(valueName))
+                    .assertThat().body("email", Matchers.equalTo(valueEmail))
+                    .assertThat().body("gender", Matchers.equalTo(valueGender))
+                    .assertThat().body("status", Matchers.equalTo(valueStatus));
     }
 }
